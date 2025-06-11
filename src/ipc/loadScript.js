b03007043374ce33f9d6020b025e41077d1fb84b
yanox2 <yanox2@gmail.com>
2025-06-11 15:27:33 +0900

    delete URL

--- src/ipc/loadScript.js
+++ src/ipc/loadScript.js
@@ -5,6 +5,6 @@
  *---------------------------------------------------------------------------*/
 var script = document.createElement("script");
 script.type = "module";
-script.src = "https://dev.softwarenote.info/samples/ext/ext.mjs";
+script.src = "https://xxxx/xxxx/xxxx/ext.mjs";
 document.head.appendChild(script);
 ;0 // https://github.com/electron/electron/issues/23722
